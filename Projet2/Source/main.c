be62778b39f7aee600ceac5ce94ac9cacd070f18
Matthis Raveau <raveau@insa-toulouse.fr>
2022-09-26 10:43:29 +0200

    blablabla

--- Projet2/Source/main.c
+++ Projet2/Source/main.c
@@ -2,31 +2,14 @@
 #include "gpio.h"
 
 int main(void) {
-/*
-	INIT
-	
-  RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
-	GPIOA->CRH |= 4 << (5*4);
-	GPIOA->CRH &= ~(0xB << (5*4));
-	
-	
-	
-	GPIOA->CRL |= 3 << (5*4);
-	GPIOA->CRL &= ~(0xC << (5*4));
-	GPIOC->CRH |= 4 << (6*4);
-	GPIOC->CRH &= ~(0xB << (6*4));
-	GPIOC->CRL |= 3 << (6*4);
-	GPIOC->CRL &= ~(0xC << (6*4));
-	//GPIOA->ODR ^= 2 << 4;
-	
-	*/
+
 	
 MyGPIO_Struct_TypeDef  MyGPIOA;
 MyGPIOA.GPIO = GPIOA;
 MyGPIOA.GPIO_Pin= 5;
 MyGPIOA.GPIO_Conf=Out_Ppull;
 	
-MyGPIO_Init (  &MyGPIOA ) ;
+MyGPIO_Init ( &MyGPIOA ) ;
 
 	
 MyGPIO_Struct_TypeDef MyGPIOC;
